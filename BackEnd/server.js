632a8ca30d778e2900c2afd988789faca834ee0c
leahgilchreest <43857385+leahgilchreest@users.noreply.github.com>
2019-12-02 11:28:54 +0000

    Dog App

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,118 +1,105 @@
 // server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = 4000;
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-const express = require('express')
-const app = express()
-const PORT = 4000
-const path = require('path')
-const bodyParser = require("body-parser");
-const mongoose =  require ("mongoose");
-
-const mongoDB = 'mongodb+srv://admin:admin@leah-lel76.mongodb.net/test?retryWrites=true&w=majority'
+const mongoDB ='mongodb+srv://admin:admin@cluster0-ku2kj.mongodb.net/grocery?retryWrites=true&w=majority';
 mongoose.connect(mongoDB, {useNewUrlParser:true});
 
 const Schema = mongoose.Schema;
 
-const dogSchema = new Schema ({
-    Name: String,
-    Breed: String, 
-    Colour: String 
+const dogSchema = new Schema({
+  Name:String,
+  Breed:String,
+  Colour:String,
 });
 
-const DogModel = mongoose.model('dogs',dogSchema);
+const DogModel = mongoose.model('dog',dogSchema);
 
-
-const cors = require('cors');
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use(function(req, res, next) {
-res.header("Access-Control-Allow-Origin", "*");
-res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-res.header("Access-Control-Allow-Headers",
-"Origin, X-Requested-With, Content-Type, Accept");
-next();
-});
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers",
+  "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+  });
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());  
-    
-app.get('/', (req, res) => res.send('Hello!'))
-
-app.get('/hello/:Name',(req, res) => {
-    console.log(req.params.Name);
-    res.send('GoodMorning! '  +  req.params.Name)
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', (req, res) => {
+  res.send('hello world');
 })
 
-    app.put('/api/dogs/:id',(req,res)=>{
-        console.log("edit" +req.params.id);
-        console.log(req.body);
-        DogModel.findByIdAndUpdate(req.params.id,req.body,{new:true},(error,data)=>{
-            res.send(data);
-        })
+app.put('/api/dogs/:id', (req, res)=>{
+  console.log(req.body);
+  console.log("Edit "+req.params.id);
+
+  DogModel.findByIdAndUpdate(req.params.id,
+    req.body, {new:true}, (error, data)=>{
+      res.send(data);
     })
-
-app.get('/Name', (req, res) => {
-    console.log('route calling');
-    console.log(req.query.firstname);
-
-    res.send('hello ' + req.query.lastname + ' ' + req.query.firstname)
-})
-
-app.get('/whatever', (req, res) => res.send('GoodBye!'))
-
-app.get ('/test', (req, res) =>{
-res.sendFile(path.join(__dirname + '/index.html'))
 })
 
 
-app.get('/api/dogs', (req, res,next) => {
- 
-    console.log ("get request") 
-    DogModel.find((err,data) =>{
-        res.json({dogs:data});
-    })   
-    })
+app.get('/api/dogs', (req,res,next) => {
 
-    app.delete('/api/dogs/:id', (req,res) =>{
-console.log(req.params.id);
+  console.log("get request")
+  DogModel.find((err,data)=>{
+    res.json({dogs:data});
+  })
+})
 
-DogModel.deleteOne({_id:req.params.id},(error,data)=>{
+
+
+
+app.delete('/api/dogs/:id', (req,res) =>{
+  console.log(req.params.id);
+
+  DogModel.deleteOne({_id:req.params.id},(error,data)=>{
     if(error)
-    res.json(error);
-        res.json(data);
+      res.json(error);
+     
+    res.json(data);
+  })
 })
-})
+
+
+
 
 app.post('/api/dogs', (req,res) =>{
-    console.log('post Sucessfull');
-    console.log(req.body)
-    console.log(req.body.Name);
-    console.log(req.body.Breed);
-    console.log(req.body.Colour);
+console.log('post Successfull');
+console.log(req.body)
+console.log(req.body.Name);
+console.log(req.body.Breed);
+console.log(req.body.Colour);
 
 
-    DogModel.create({
-        Name:req.body.Name,
-        Breed:req.body.Breed,
-        Colour:req.body.Colour
-    });
 
-        res.json('data uploaded');
+DogModel.create({
+  Name: req.body.Name,
+  Breed: req.body.Breed,
+  Colour: req.body.Colour
 
-    })
- 
+});
+res.json('data uploaded')
 
-app.post('/name', (req, res) => {
-    console.log("post method");
-    console.log(req.body.firstname);
-    res.send('Hello ' + req.body.firstname + " " + req.body.lastname);
-    })
-    
-app.get('/api/dogs/:id',(req,res)=>{
-    console.log(req.params.id);
-
-    DogModel.findById(req.params.id ,(err,data)=>{
-  res.json(data);
-    })
 })
+
+app.get('/api/dogs/:id',(req,res)=>{
+  console.log(req.params.id);
+
+  DogModel.findById(req.params.id, (err, data)=>{
+    res.json(data);
+  })
+})
+
+
 
 app.listen(PORT, () => 
 console.log(`Example app listening on port ${PORT}!`))
